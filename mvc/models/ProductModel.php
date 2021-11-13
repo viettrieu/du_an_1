488061e25e79f91ede0ff9ba1c9816091fdcaf27
viet trieu <trieunvvps17073@fpt.edu.vn>
2021-11-13 12:53:31 +0700

    add book, author, tag, category

--- mvc/models/ProductModel.php
+++ mvc/models/ProductModel.php
@@ -1,41 +1,41 @@
 <?php
 class ProductModel extends DB
 {
-    public $table = "ps_product";
-    // public function GetAllProduct()
-    // {
-    //     $sql = "SELECT product.*, X.rating FROM ps_product product LEFT JOIN (SELECT productId, AVG(rating) AS 'rating' FROM ps_product_review WHERE status = 1 GROUP BY productId) X  ON X.productId = product.id ";
-    //     return $this->pdo_query($sql);
-    // }
-    // public function GetAllProducts()
-    // {
-    //     $sql = "SELECT * FROM ps_product";
-    //     return $this->pdo_query($sql);
-    // }
+    public $table = "book";
+    public function GetAllProduct()
+    {
+        $sql = "SELECT product.*, X.rating FROM ps_product product LEFT JOIN (SELECT productId, AVG(rating) AS 'rating' FROM ps_product_review WHERE status = 1 GROUP BY productId) X  ON X.productId = product.id ";
+        return $this->pdo_query($sql);
+    }
+    public function GetAllProducts()
+    {
+        $sql = "SELECT * FROM book";
+        return $this->pdo_query($sql);
+    }
     public function GetSellProduct($limit = 6)
     {
-        $sql = "SELECT product.id,product.title, thumbnail, product.price, SUM(orderi.quantity) AS 'quantity', X.rating FROM ps_product product LEFT JOIN (SELECT productId, AVG(rating) AS 'rating' FROM ps_product_review  WHERE status = 1 GROUP BY productId) X  ON X.productId = product.id JOIN ps_order_item orderi   ON product.id = orderi.productId GROUP BY orderi.productId ORDER BY quantity DESC LIMIT $limit";
+        $sql = "SELECT product.id,product.title, thumbnail, product.price, SUM(orderi.quantity) AS 'quantity', X.rating FROM book product LEFT JOIN (SELECT productId, AVG(rating) AS 'rating' FROM book_review  WHERE status = 1 GROUP BY productId) X  ON X.productId = product.id JOIN order_item orderi   ON product.id = orderi.productId GROUP BY orderi.productId ORDER BY quantity DESC LIMIT $limit";
         return $this->pdo_query($sql);
     }
     public function GetHotProduct($limit = 6)
     {
-        $sql = "SELECT product.id,product.title, thumbnail,product.price, SUM(orderi.productId) AS 'quantity', X.rating FROM ps_product product LEFT JOIN (SELECT productId, AVG(rating) AS 'rating' FROM ps_product_review  WHERE status = 1 GROUP BY productId) X  ON X.productId = product.id  INNER JOIN ps_order_item orderi ON product.id = orderi.productId GROUP BY orderi.productId  ORDER BY orderi.productId DESC LIMIT $limit";
+        $sql = "SELECT product.id,product.title, thumbnail,product.price, SUM(orderi.productId) AS 'quantity', X.rating FROM book product LEFT JOIN (SELECT productId, AVG(rating) AS 'rating' FROM book_review  WHERE status = 1 GROUP BY productId) X  ON X.productId = product.id  INNER JOIN order_item orderi ON product.id = orderi.productId GROUP BY orderi.productId  ORDER BY orderi.productId DESC LIMIT $limit";
         return $this->pdo_query($sql);
     }
     public function GetViewProduct($limit = 6)
     {
-        $sql = "SELECT product.id,product.title, thumbnail,product.price, CAST(mt.content AS int) AS 'view',  X.rating FROM ps_product product LEFT JOIN (SELECT productId, AVG(rating) AS 'rating' FROM ps_product_review  WHERE status = 1 GROUP BY productId) X  ON X.productId = product.id  INNER JOIN ps_product_meta mt ON product.id = mt.productId AND `key` = 'view' ORDER BY view DESC  LIMIT $limit";
+        $sql = "SELECT product.id,product.title, thumbnail,product.price, CAST(mt.content AS int) AS 'view',  X.rating FROM book product LEFT JOIN (SELECT productId, AVG(rating) AS 'rating' FROM book_review  WHERE status = 1 GROUP BY productId) X  ON X.productId = product.id  INNER JOIN ps_product_meta mt ON product.id = mt.productId AND `key` = 'view' ORDER BY view DESC  LIMIT $limit";
         return $this->pdo_query($sql);
     }
     public function GetByTaxonomy($id = 0, $name = 0, $offset = 0, $perPage = 0)
     {
-        $sql = "SELECT product.*, X.rating FROM ps_product product LEFT JOIN (SELECT productId, AVG(rating) AS 'rating' FROM ps_product_review WHERE status = 1 GROUP BY productId) X  ON X.productId = product.id ";
+        $sql = "SELECT product.*, X.rating FROM book product LEFT JOIN (SELECT productId, AVG(rating) AS 'rating' FROM book_review WHERE status = 1 GROUP BY productId) X  ON X.productId = product.id ";
         if ($id != 0) {
             if ($name == 'tag') {
-                $sql .= "INNER JOIN product_tag tag ON tagId = $id AND product.id = tag.productId";
+                $sql .= "INNER JOIN book_tag tag ON tagId = $id AND product.id = tag.productId";
             }
             if ($name == 'category') {
-                $sql .= "INNER JOIN product_category category ON categoryId = $id AND product.id = category.productId";
+                $sql .= "INNER JOIN book_category category ON categoryId = $id AND product.id = category.productId";
             }
             if ($name == 'search') {
                 $sql .= "WHERE (title LIKE '%$id%' OR summary LIKE '%$id%')";
@@ -47,29 +47,31 @@ class ProductModel extends DB
     }
     public function GetProductById($id)
     {
-        $sql = "SELECT * FROM ps_product WHERE id = $id";
+        $sql = "SELECT * FROM book WHERE id = $id";
         return $this->pdo_query_one($sql);
     }
-    public function SumViewById($id)
-    {
-        $sql = "SELECT content FROM ps_product_meta WHERE productId = $id AND `key` = 'view'";
-        return $this->pdo_query_value($sql);
-    }
-    public function CountViewById($id)
-    {
-        $sql = "INSERT INTO ps_product_meta (productId, `key`, content) VALUES ('$id', 'view', 1)  ON DUPLICATE KEY UPDATE content = content + 1";
-        return $this->pdo_execute($sql);
-    }
+    // //
+    // public function SumViewById($id)
+    // {
+    //     $sql = "SELECT content FROM ps_product_meta WHERE productId = $id AND `key` = 'view'";
+    //     return $this->pdo_query_value($sql);
+    // }
+    // //
+    // public function CountViewById($id)
+    // {
+    //     $sql = "INSERT INTO ps_product_meta (productId, `key`, content) VALUES ('$id', 'view', 1)  ON DUPLICATE KEY UPDATE content = content + 1";
+    //     return $this->pdo_execute($sql);
+    // }
     public function GetRelatedProductById($id, $num)
     {
-        $sql = "SELECT product.id, thumbnail, product.title, price, X.rating, categoryId FROM ps_product product
-        LEFT JOIN (SELECT productId, AVG(rating) AS 'rating' FROM ps_product_review WHERE status = 1 GROUP BY productId) X  ON X.productId = product.id
-        LEFT JOIN product_category category ON product.id = category.productId WHERE categoryId IN( SELECT id FROM ps_category INNER JOIN product_category ON categoryId = ps_category.id WHERE productId = $id) AND NOT product.id = $id GROUP BY  product.id ORDER BY RAND() LIMIT $num";
+        $sql = "SELECT product.id, thumbnail, product.title, price, X.rating, categoryId FROM book product
+        LEFT JOIN (SELECT productId, AVG(rating) AS 'rating' FROM book_review WHERE status = 1 GROUP BY productId) X  ON X.productId = product.id
+        LEFT JOIN book_category category ON product.id = category.productId WHERE categoryId IN( SELECT id FROM category INNER JOIN book_category ON categoryId = category.id WHERE productId = $id) AND NOT product.id = $id GROUP BY  product.id ORDER BY RAND() LIMIT $num";
         return $this->pdo_query($sql);
     }
     public function Check($id)
     {
-        $sql = "SELECT id FROM ps_product WHERE id = $id";
+        $sql = "SELECT id FROM book WHERE id = $id";
         return $this->pdo_query_one($sql);
     }
     public function InsertProduct($data)
@@ -82,15 +84,27 @@ class ProductModel extends DB
     }
     public function InsertProductCategory($data)
     {
-        return $this->insert("product_category", $data);
+        return $this->insert("book_category", $data);
     }
     public function UpdateProductCategory($data, $cond)
     {
-        return $this->update("product_category", $data, $cond);
+        return $this->update("book_category", $data, $cond);
     }
     public function InsertProductTag($data)
     {
-        return $this->insert("product_tag", $data);
+        return $this->insert("book_tag", $data);
+    }
+    public function DeleteProductTag($cond = 1)
+    {
+        return $this->delete("book_tag", $cond);
+    }
+    public function InsertProductAuthor($data)
+    {
+        return $this->insert("book_author", $data);
+    }
+    public function DeleteProductAuthor($cond = 1)
+    {
+        return $this->delete("book_author", $cond);
     }
     public function DeleteProductById($cond)
     {
--- mvc/views/admin/pages/create-product.php
+++ mvc/views/admin/pages/create-product.php
@@ -1,120 +1,128 @@
 <div class="page-wrapper">
-  <div class="content container-fluid">
+    <div class="content container-fluid">
 
-    <!-- Page Header -->
-    <div class="page-header">
-      <div class="row">
-        <div class="col-sm-12">
-          <h3 class="page-title">Sản phẩm</h3>
-          <ul class="breadcrumb">
-            <li class="breadcrumb-item active">Tạo sản phẩm</li>
-          </ul>
+        <!-- Page Header -->
+        <div class="page-header">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3 class="page-title">Sản phẩm</h3>
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item active">Tạo sản phẩm</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <!-- /Page Header -->
+        <!-- /Page Header -->
 
-    <form method="POST" id="create_product" enctype="multipart/form-data" class="needs-validation" novalidate>
-      <div class="row">
-        <div class="col-md-9">
-          <div class="card">
-            <div class="card-body">
-              <?php foreach ($data["Errors"] as $error) :
-                $class = $error["status"] == "ERROR" ? "alert-danger" : "alert-success";
-              ?>
-              <div class="alert <?= $class ?> alert-dismissible fade show" role="alert">
-                <?= $error["message"] ?>
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                  <span aria-hidden="true">×</span>
-                </button>
-              </div>
-              <?php endforeach ?>
-              <div class="form-group ">
-                <label>Tên sản phẩm:</label>
-                <input type="text" class="form-control" id="title" required name="title">
-                <div class=" invalid-feedback">Vui lòng nhập tên sản phẩm.
-                </div>
-              </div>
-              <div class="form-group">
-                <label>Mô tả chi tiết:</label>
-                <div id="content"></div>
-                <input type="hidden" name="content">
-              </div>
-              <div class="form-group">
-                <label>Mô tả ngắn:</label>
-                <div id="summary"></div>
-                <input type="hidden" name="summary">
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <div class="form-group">
-                <label>Danh mục sản phẩm:</label>
-                <select name="category" class="select custom-select" required>
-                  <option value="" selected>Chọn danh mục sản phẩm</option>
-                  <?php foreach ($data["ListCategory"] as $category) : ?>
-                  <option value="<?= $category['id'] ?>"><?= $category['title'] ?></option>
-                  <?php endforeach ?>
-                </select>
-              </div>
-              <div class="form-group">
-                <label>Từ khóa sản phẩm:</label>
-                <select name="tag[]" class="select2tag" multiple="multiple">
-                  <?php foreach ($data["ListTag"] as $tag) : ?>
-                  <option value="<?= $tag['id'] ?>"><?= $tag['title'] ?></option>
-                  <?php endforeach ?>
-                </select>
-              </div>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body">
-              <div class="form-group">
-                <label>Giá bán thường:</label>
-                <div class="input-group ">
-                  <input type="number" class="form-control" id="price" required name="price">
-                  <div class="input-group-append">
-                    <span class="input-group-text">VNĐ</span>
-                  </div>
-                  <div class="invalid-feedback">Vui lòng nhập giá bán sản phẩm.</div>
-                </div>
-              </div>
-              <div class="form-group">
-                <label>Giá khuyến mãi:</label>
-                <div class="input-group">
-                  <input type="number" class="form-control" id="discount" name="discount">
-                  <div class="input-group-append">
-                    <span class="input-group-text">VNĐ</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body">
-              <div class="form-group">
-                <label>Ảnh sản phẩm</label>
-                <div class="thumbnail-upload">
-                  <div class="thumbnail-edit">
-                    <input type="file" id="thumbnail" name="thumbnail" accept=".png, .jpg, .jpeg" />
-                    <label for="thumbnail"><i class="fas fa-pencil-alt"></i></label>
-                  </div>
-                  <div class="thumbnail-preview">
-                    <div id="thumbnailPreview"
-                      style="background-image: url(<?= SITE_URL ?>/public/img/default-product-image.png)">
+        <form method="POST" id="create_product" enctype="multipart/form-data" class="needs-validation" novalidate>
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="card">
+                        <div class="card-body">
+                            <?php foreach ($data["Errors"] as $error) :
+                                $class = $error["status"] == "ERROR" ? "alert-danger" : "alert-success";
+                            ?>
+                            <div class="alert <?= $class ?> alert-dismissible fade show" role="alert">
+                                <?= $error["message"] ?>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <?php endforeach ?>
+                            <div class="form-group ">
+                                <label>Tên sản phẩm:</label>
+                                <input type="text" class="form-control" id="title" required name="title">
+                                <div class=" invalid-feedback">Vui lòng nhập tên sản phẩm.
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label>Mô tả chi tiết:</label>
+                                <div id="content"></div>
+                                <input type="hidden" name="content">
+                            </div>
+                            <div class="form-group">
+                                <label>Mô tả ngắn:</label>
+                                <div id="summary"></div>
+                                <input type="hidden" name="summary">
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Danh mục sản phẩm:</label>
+                                <select name="category" class="select custom-select" required>
+                                    <option value="" selected>Chọn danh mục sản phẩm</option>
+                                    <?php foreach ($data["ListCategory"] as $category) : ?>
+                                    <option value="<?= $category['id'] ?>"><?= $category['title'] ?></option>
+                                    <?php endforeach ?>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Từ khóa sản phẩm:</label>
+                                <select name="tag[]" class="select2tag" multiple="multiple">
+                                    <?php foreach ($data["ListTag"] as $tag) : ?>
+                                    <option value="<?= $tag['id'] ?>"><?= $tag['title'] ?></option>
+                                    <?php endforeach ?>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Tác giả:</label>
+                                <select name="author[]" class="select2tag" multiple="multiple">
+                                    <?php foreach ($data["ListAuthor"] as $author) : ?>
+                                    <option value="<?= $author['id'] ?>"><?= $author['title'] ?></option>
+                                    <?php endforeach ?>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Giá bán thường:</label>
+                                <div class="input-group ">
+                                    <input type="number" class="form-control" id="price" required name="price">
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">VNĐ</span>
+                                    </div>
+                                    <div class="invalid-feedback">Vui lòng nhập giá bán sản phẩm.</div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label>Giá khuyến mãi:</label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control" id="discount" name="discount">
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">VNĐ</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Ảnh sản phẩm</label>
+                                <div class="thumbnail-upload">
+                                    <div class="thumbnail-edit">
+                                        <input type="file" id="thumbnail" name="thumbnail" accept=".png, .jpg, .jpeg" />
+                                        <label for="thumbnail"><i class="fas fa-pencil-alt"></i></label>
+                                    </div>
+                                    <div class="thumbnail-preview">
+                                        <div id="thumbnailPreview"
+                                            style="background-image: url(<?= SITE_URL ?>/public/img/default-product-image.png)">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" name="create_product" class="btn btn-block btn-primary btn-lg">Đăng sản
+                        phẩm</button>
+                </div>
             </div>
-          </div>
-          <button type="submit" name="create_product" class="btn btn-block btn-primary btn-lg">Đăng sản
-            phẩm</button>
-        </div>
-      </div>
-    </form>
-  </div>
+        </form>
+    </div>
 </div>
