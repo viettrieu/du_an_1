d14adc1ddb634fd0f6ab2a62c49d9699fe92e830
Nghia19 <trungnghia191919@gmail.com>
2021-11-24 20:24:38 +0700

    updata pass mail

--- mvc/core/Helper.php
+++ mvc/core/Helper.php
@@ -36,7 +36,7 @@ class Helper
       $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
       $mail->SMTPAuth = true; //Enable SMTP authentication
       $mail->Username = 'trungnghia191919@gmail.com'; //SMTP username
-      $mail->Password = 'trryunuvpshnmryr'; //SMTP password
+      $mail->Password = 'muwprjepewtknlwh'; //SMTP password
       $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
       $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
       $mail->CharSet = 'UTF-8';
