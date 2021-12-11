17455ba79ae5bfa3bef5eebaaaabb649a01e2221
Nghia19 <trungnghia191919@gmail.com>
2021-12-11 15:52:04 +0700

    fix  search form (focus)

--- public/js/searh.js
+++ public/js/searh.js
@@ -2,5 +2,5 @@ $(document).on("click", ".sea", function (e) {
   $("#modal-7").addClass("md-show");
   setTimeout(() => {
     document.querySelector("#modal-7 .search-field").focus();
-  }, 30);
+  }, 100);
 });
