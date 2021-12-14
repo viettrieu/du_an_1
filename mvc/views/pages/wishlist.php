72a9856e9a814a4e5fcd3e182cfbf43f747a0f21
Nghia19 <trungnghia191919@gmail.com>
2021-12-14 22:34:33 +0700

    remove Số Lượng (wishlist)

--- mvc/views/pages/wishlist.php
+++ mvc/views/pages/wishlist.php
@@ -31,55 +31,51 @@
 
   <div class="large-9 col">
     <?php if (($number = count($data['Items'])) > 0) { ?>
-      <table class="cart table-form shop_table yproduct" cellspacing="0">
-        <thead>
-          <tr>
-            <th class="product-remove"></th>
-            <th class="product-thumbnail"></th>
-            <th class="product-name">Hàng hóa</th>
-            <th class="product-price">Đơn giá</th>
-            <th class="product-subtotal">Số Lượng</th>
-            <th class="add-to-card"></th>
+    <table class="cart table-form shop_table yproduct" cellspacing="0">
+      <thead>
+        <tr>
+          <th class="product-remove"></th>
+          <th class="product-thumbnail"></th>
+          <th class="product-name">Hàng hóa</th>
+          <th class="product-price">Đơn giá</th>
+          <th class="add-to-card"></th>
 
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach ($data['Items'] as $key => $item) { ?>
-            <tr class="cart-item" data-id="<?= $item["id"] ?>">
-              <td class="product-remove">
-                <i class="far fa-times-circle remove" onclick="removeItemFromWishlist(this)"></i>
-              </td>
-              <td class="product-thumbnail">
-                <a href="<?= SITE_URL ?>/store/product/<?= $item["id"] ?>">
-                  <img width="90" height="90" src="<?= SITE_URL ?><?= $item['thumbnail'] ?>">
-                </a>
-              </td>
-              <td class="product-name">
-                <a href="<?= SITE_URL ?>/store/product/<?= $item["id"] ?>"><?= $item['title'] ?></a>
-              </td>
-              <td class="product-price" data-title="Giá" data-price="<?= $item['price'] ?>">
-                <span class="price">
-                  <span> <?= number_format($item['price']) ?></span>
-                  ₫
-                </span>
-              </td>
-              <td class="product-quantity" data-title="Số lượng">
-                100
-              </td>
-              <td>
-                <div class="add-the-cart">
-                  <a href="#" class="secondary" data-id="<?= $item["id"] ?>">
-                    <i class="fas fa-shopping-cart"></i>
-                    <i class="fas fa-book"></i>
-                    <span>THÊM VÀO GIỎ</span>
-                  </a>
-                </div>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($data['Items'] as $key => $item) { ?>
+        <tr class="cart-item" data-id="<?= $item["id"] ?>">
+          <td class="product-remove">
+            <i class="far fa-times-circle remove" onclick="removeItemFromWishlist(this)"></i>
+          </td>
+          <td class="product-thumbnail">
+            <a href="<?= SITE_URL ?>/store/product/<?= $item["id"] ?>">
+              <img width="90" height="90" src="<?= SITE_URL ?><?= $item['thumbnail'] ?>">
+            </a>
+          </td>
+          <td class="product-name">
+            <a href="<?= SITE_URL ?>/store/product/<?= $item["id"] ?>"><?= $item['title'] ?></a>
+          </td>
+          <td class="product-price" data-title="Giá" data-price="<?= $item['price'] ?>">
+            <span class="price">
+              <span> <?= number_format($item['price']) ?></span>
+              ₫
+            </span>
+          </td>
+          <td>
+            <div class="add-the-cart">
+              <a href="#" class="secondary" data-id="<?= $item["id"] ?>">
+                <i class="fas fa-shopping-cart"></i>
+                <i class="fas fa-book"></i>
+                <span>THÊM VÀO GIỎ</span>
+              </a>
+            </div>
 
-              </td>
-            </tr>
-          <?php } ?>
-        </tbody>
-      </table>
+          </td>
+        </tr>
+        <?php } ?>
+      </tbody>
+    </table>
     <?php } ?>
     <div class="nproduct" style="display: <?= $number > 0 ? "none" : "block" ?>">
       <div class="text-center">
