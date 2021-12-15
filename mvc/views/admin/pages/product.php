d70add541602e4abd54083b66f6528a2bfe3d539
Nghia19 <trungnghia191919@gmail.com>
2021-12-15 15:27:40 +0700

    remove quantily (Product admin)

--- mvc/views/admin/pages/product.php
+++ mvc/views/admin/pages/product.php
@@ -36,7 +36,6 @@
                     <th>Tên</th>
                     <th>Tác giả</th>
                     <th>NXB</th>
-                    <th>Kho</th>
                     <th>Giá</th>
                     <th class="text-right">Action</th>
                   </tr>
@@ -46,42 +45,42 @@
                   $listProduct = $data["ListProduct"];
                   if (isset($listProduct)) {
                     foreach ($listProduct as $product) { ?>
-                      <tr>
-                        <td class="id"><?= $product['id']; ?></td>
-                        <td>
-                          <h2 class="table-avatar">
-                            <a href="<?= SITE_URL ?>/store/product/<?= $product['id']; ?>"><img class="avatar avatar-lg mr-2 avatar-img rounded" src="<?= SITE_URL ?><?= $product['thumbnail'] == null ? '/public/img/default-product-image.png' : $product['thumbnail']; ?>" alt="<?= $product['title']; ?>">
-                              <span class="title"><?= $product['title']; ?></span>
-                            </a>
-                          </h2>
-                        </td>
-                        <td>
-                          <?php
+                  <tr>
+                    <td class="id"><?= $product['id']; ?></td>
+                    <td>
+                      <h2 class="table-avatar">
+                        <a href="<?= SITE_URL ?>/store/product/<?= $product['id']; ?>"><img
+                            class="avatar avatar-lg mr-2 avatar-img rounded"
+                            src="<?= SITE_URL ?><?= $product['thumbnail'] == null ? '/public/img/default-product-image.png' : $product['thumbnail']; ?>"
+                            alt="<?= $product['title']; ?>">
+                          <span class="title"><?= $product['title']; ?></span>
+                        </a>
+                      </h2>
+                    </td>
+                    <td>
+                      <?php
                           $author = json_decode('[' . $product["author"] . ']', true);
                           echo implode(", ", array_column($author, "title"))
                           ?>
-                        </td>
-                        <td><?= $product['publisher']; ?></td>
-                        <td class="status">
-                          <span class="badge-pill bg-<?= $product['quantity'] == null || $product['quantity']  > 10 ? 'success' : ($product['quantity'] == 0 ? 'danger' : 'warning'); ?>-light">
-                            <?= $product['quantity'] == null || $product['quantity']  > 10 ? 'Còn hàng' : ($product['quantity'] == 0 ? 'Hết hàng' : 'Sắp hết'); ?>
-                          </span>
-                        </td>
-                        <td>
-                          <?php if (isset($product["discount"])) : ?>
-                            <del aria-hidden="true">
-                              <span><?= number_format($product["price"], 0, ',', '.') ?>₫</span>
-                            </del>
-                          <?php endif ?>
-                          <ins class="sizeprice-1">
-                            <span><?= number_format(isset($product["discount"]) ? $product["discount"] : $product["price"], 0, ',', '.') ?>₫</span>
-                          </ins>
-                        </td>
-                        <td class="text-right">
-                          <a href="<?= ADMIN_URL ?>/product/edit/<?= $product['id']; ?>" class="btn btn-sm btn-white text-success mr-2"><i class="far fa-edit mr-1"></i>Sửa</a>
-                          <a href="javascript:void(0);" class="btn btn-sm btn-white text-danger delete"><i class="far fa-trash-alt mr-1"></i>Xóa</a>
-                        </td>
-                      </tr>
+                    </td>
+                    <td><?= $product['publisher']; ?></td>
+                    <td>
+                      <?php if (isset($product["discount"])) : ?>
+                      <del aria-hidden="true">
+                        <span><?= number_format($product["price"], 0, ',', '.') ?>₫</span>
+                      </del>
+                      <?php endif ?>
+                      <ins class="sizeprice-1">
+                        <span><?= number_format(isset($product["discount"]) ? $product["discount"] : $product["price"], 0, ',', '.') ?>₫</span>
+                      </ins>
+                    </td>
+                    <td class="text-right">
+                      <a href="<?= ADMIN_URL ?>/product/edit/<?= $product['id']; ?>"
+                        class="btn btn-sm btn-white text-success mr-2"><i class="far fa-edit mr-1"></i>Sửa</a>
+                      <a href="javascript:void(0);" class="btn btn-sm btn-white text-danger delete"><i
+                          class="far fa-trash-alt mr-1"></i>Xóa</a>
+                    </td>
+                  </tr>
                   <?php }
                   }
                   ?>
